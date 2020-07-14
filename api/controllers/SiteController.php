a5ae1d749536ed2b7b3bc292ead741a0c33041e5
yongkun.xiong <weilone@vip.qq.com>
2020-07-14 09:00:43 +0800

    ++

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -56,7 +56,7 @@ class SiteController extends \yii\rest\ActiveController
      */
     public function actionIndex()
     {
-//        $data['allCate'] = CourseCate::find()->select(['name', 'id', 'alias_name'])->all();
+        $data['allCate'] = CourseCate::find()->select(['name', 'id', 'alias_name'])->asArray()->all();
         // 推荐课程
         $data['banner'] = [];
         $banner = Options::find()->asArray()->where(['name' => 'index'])->select('value')->one();
